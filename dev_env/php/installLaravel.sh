4ba6b1b44da69caaae48c1bda05cc93319a92bb2
Peter Cheng <teed7334@163.com>
2020-05-11 14:31:35 +0800

    追加blueprint

--- dev_env/php/installLaravel.sh
+++ dev_env/php/installLaravel.sh
@@ -5,4 +5,5 @@ cd /var/www/html/web;
 chmod -R 777 storage bootstrap/cache;
 php ../composer.phar require tcg/voyager;
 php ../composer.phar require barryvdh/laravel-debugbar --dev
+php ../composer.phar require --dev laravel-shift/blueprint
 echo 'success';
